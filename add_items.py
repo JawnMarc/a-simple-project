b7246640275eb93848820e51c898be5f57c57e5f
JawnMarc <awukum18@gmail.com>
2021-10-22 12:16:03 +0000

    Added feature add_items and component

--- add_items.py
+++ add_items.py
@@ -1,7 +1,8 @@
 def add_values(a, b):
-  return a+b
+    return a+b
 
 
 
 if __name__ == '__main__':
-
+    RES = add_values(10, 10)
+    print(RES)
--- component.py
+++ component.py
@@ -1,7 +1,22 @@
-import math
-import pandas
+'''
+Author: Mark
+Date: 21/20/21
+'''
 
+
+def butyler_function(param1, param2):
+    '''
+    Function takes in ...
+    Args:
+        param1 (str):
+        param2 (str):
+
+    Return:
+        Function returns the value of ...
+    '''
+    res = [para*param2 for para in param1 if para < -1]
+    return res
 
 
 if __name__ == '__main__':
-
+    pass
